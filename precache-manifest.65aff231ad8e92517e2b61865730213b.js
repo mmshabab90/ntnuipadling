78571abd039768e1049983fee04f0acc6791449c
Meeyad Mohd. Shabab <mmshabab90@gmail.com>
2020-08-08 14:35:32 +0200

    Updates

---
+++ precache-manifest.65aff231ad8e92517e2b61865730213b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b6016f0fe7d30326dbf2509bbbae91",
+    "url": "/ntnuipadling/index.html"
+  },
+  {
+    "revision": "058315db7156cc20ea93",
+    "url": "/ntnuipadling/static/css/main.4731f16e.chunk.css"
+  },
+  {
+    "revision": "d446b1f3272cd9d55578",
+    "url": "/ntnuipadling/static/js/2.00ee9c4d.chunk.js"
+  },
+  {
+    "revision": "697860c8813b3ec16b626c5554675bbb",
+    "url": "/ntnuipadling/static/js/2.00ee9c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058315db7156cc20ea93",
+    "url": "/ntnuipadling/static/js/main.8ecc9e8f.chunk.js"
+  },
+  {
+    "revision": "d1f2bfe9510dd8922279",
+    "url": "/ntnuipadling/static/js/runtime-main.4d897e34.js"
+  },
+  {
+    "revision": "5842b6a0d6f631dc6e84038a2ef10583",
+    "url": "/ntnuipadling/static/media/Error404.5842b6a0.png"
+  }
+]);
